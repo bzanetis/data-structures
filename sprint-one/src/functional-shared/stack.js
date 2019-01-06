c5c58ef16bef9da8e8e7e93b15d11bc667d39e64
bzanetis <britt.zanetis19@gmail.com>
2019-01-05 21:30:47 -0600

    Data Structures progress - functional shared stack, all tests passing

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  // Use an object with numeric ks to store values
+  var storage = {};
+  // storage.value = arguments[0];
+  someInstance.count = Object.keys(storage).length;
+  someInstance.storage = storage;
+
+  extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var extend = function(obj, methods){
+  for(var k in methods){
+    obj[k] = methods[k];
+  }
+}
 
-
+var stackMethods = {
+  pop: function(){
+    var popped = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return popped;
+  },
+  push: function(value){
+    this.count++;
+    this.storage[this.count] = value;
+  },
+  size: function(){
+    if(this.count < 0){
+      return 0;
+    }
+    return this.count;
+  }
+};
