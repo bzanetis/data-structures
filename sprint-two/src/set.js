031622b817619206b9e7749cd79f4bc56e561835
bzanetis <britt.zanetis19@gmail.com>
2019-01-08 18:16:12 -0600

    Data structures, set - all tests passing

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
+};
+
+var extend = function(obj, methods) {
+  for (var k in methods) {
+    obj[k] = methods[k];
+  }
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  if (this.storage.includes(item)) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (this.storage.includes(item)) {
+    var donna = this.storage.indexOf(item);
+    this.storage.splice(donna);
+  }
 };
 
 /*
